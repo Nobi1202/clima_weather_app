e94d34c1ccd8b3b62c7b52be473fe38feb5afe9a
Nobi1202 <saobang001111@gmail.com>
2021-11-19 00:18:40 +0700

    update: full app

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,22 +1,18 @@
-import 'package:clima/screens/location_screen.dart';
-import 'package:clima/services/location.dart';
-import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'location_screen.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-const apiKey = 'a3b2622d2048245fdf154dde09b0df55';
+import 'package:clima/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
 
   @override
-  _LoadingScreenState createState() => _LoadingScreenState();
+  State<StatefulWidget> createState() {
+    return _LoadingScreenState();
+  }
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  double? latitude;
-  double? longitude;
-
   @override
   void initState() {
     super.initState();
@@ -24,25 +20,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-    longitude = location.longitude!;
-    latitude = location.latitude!;
-
-    getData();
-  }
-
-  void getData() async {
-    NetworkHelper networkHelper = NetworkHelper(
-        'https://samples.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
-
-    var weatherData = await networkHelper.getData();
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) {
-          return const LocationScreen();
+          return LocationScreen(
+            locationWeather: weatherData,
+          );
         },
       ),
     );
@@ -50,12 +36,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return const Scaffold(
       body: Center(
         child: SpinKitDoubleBounce(
           color: Colors.white,
-          size: 100,
+          size: 100.0,
         ),
       ),
     );
--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -13,13 +13,9 @@ class NetworkHelper {
     if (response.statusCode == 200) {
       String data = response.body;
 
-      var decodedData = jsonDecode(data);
-
-      return decodedData;
-
+      return jsonDecode(data);
     } else {
       print(response.statusCode);
     }
   }
 }
-
